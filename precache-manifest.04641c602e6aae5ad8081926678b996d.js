1b94503556ed43e5953ff181bb16207d1e018611
S. Jordan <curricle@gmail.com>
2019-11-17 18:34:20 -0500

    Updates

---
+++ precache-manifest.04641c602e6aae5ad8081926678b996d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96d5cbca9b67a50d59e591cf562824da",
+    "url": "/FR-Rebuild/index.html"
+  },
+  {
+    "revision": "4912ee15c4ddfb1e22e7",
+    "url": "/FR-Rebuild/static/css/main.90cd01b6.chunk.css"
+  },
+  {
+    "revision": "983703285719ac710833",
+    "url": "/FR-Rebuild/static/js/2.97edfbb0.chunk.js"
+  },
+  {
+    "revision": "4912ee15c4ddfb1e22e7",
+    "url": "/FR-Rebuild/static/js/main.07ef5cfc.chunk.js"
+  },
+  {
+    "revision": "f194994dbec4f548a568",
+    "url": "/FR-Rebuild/static/js/runtime-main.ba598e5b.js"
+  }
+]);
